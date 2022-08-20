5311f7afd3b6839c0eda053a9d484e1adcaf14b3
Sufiyan-khanzada <Sufiyan-khanzada>
2022-08-21 04:37:26 +0500

    Advance Working

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ include 'header.php';
           <div class="row d-flex justify-content-center">
             <div class="col-lg-6 text-center">
               <h2>Contact</h2>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+              <p>EasyTrans Connecting Your Heart</p>
             </div>
           </div>
         </div>
